c71770acb9783ef2a985ac415225fe83085b1419
ecbrudner <148579054+ecbrudner@users.noreply.github.com>
2023-11-15 21:34:00 -0600

    added password length prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,22 @@
 // Assignment code here
-
+function generatePassword(){
+  //prompt for password length 8-128
+  var passwordLength= prompt("Enter password length (8-128 characters)", "Enter password length");
+  if (passwordLength.length < 8){
+    alert("Password must be at least 8 characters in length.");
+  } else if (passwordLength.length > 128){
+    alert("Password length can be no longer than 128 characters.");
+  } else {
+    alert("Great, thank you!");
+  }
+  //lowercase? t/f
+  //uppercase? t/f
+  //numeric? t/f
+  //special char t/f
+  //create pw character bank based on answers
+  //generate random pw w loop for length
+  //display pw
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
